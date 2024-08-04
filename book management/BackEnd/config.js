33d095a23d83359059a60fbaba0ee6eddd5c8308
Suruchi Wagh <suruchiwagh614@gmail.com>
2024-08-04 18:29:52 +0530

    card functianality added

--- book management/BackEnd/config.js
+++ book management/BackEnd/config.js
@@ -1,5 +1,6 @@
 export const PORT = 5555 ;
 
 export const mongoDBURL = 
-"mongodb+srv://root:root@books-store-mern.l9mpuqb.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN"
+// "mongodb+srv://root:root@books-store-mern.l9mpuqb.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN"
+"mongodb+srv://root:root@books-store-mern.wmcbgdv.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN"
 // Create free database for this thing and it will be deleted after this is done .//
